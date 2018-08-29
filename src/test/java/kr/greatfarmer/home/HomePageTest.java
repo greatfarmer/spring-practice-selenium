4620ee32ae27f942b1212f2e1e002de8aa029317
Seonghun <lutis107@gmail.com>
2018-08-29 16:45:21 +0900

    Modify test code using JUnit

--- src/test/java/kr/greatfarmer/home/HomePageTest.java
+++ src/test/java/kr/greatfarmer/home/HomePageTest.java
@@ -2,17 +2,44 @@ package kr.greatfarmer.home;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePageTest {
-	public static void main(String[] args) {
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		// ChromeDriver 위치
 		System.setProperty("webdriver.chrome.driver", "src/test/driver/chromedriver/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver(); // Driver 생성
+	}
+	
+	@Test
+	public void test() {
 		String baseUrl = "http://corin2.site/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get(baseUrl);
+		driver.get(baseUrl); // URL로 접속
+		// CSS선택자로 Element 가져오기
 		driver.findElement(By.cssSelector("div#myNavbar > ul.nav.navbar-nav.navbar-right > li:nth-child(1)")).click();
+		
+		// id명으로 Element 가져오기
+		// driver.findElement(By.id("id"));
+		
+		// class명으로 Element 가져오기
+		// driver.findElement(By.className("className"));
+		
+		// tag명으로 Element 가져오기
+		// driver.findElement(By.tagName("tagName"));
+		
+	}
+	
+	@After
+	public void quitTest() {
+		driver.quit(); // Driver 종료
 	}
 }
